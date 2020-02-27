d8c0765534f1b0f28f27f050dcc89c0dc9ad4457
BlackSync <mixa.mac2011@yandex.ru>
2020-02-27 15:26:37 +0700

    create a app

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "functons.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+int x,y;
+float r;
+scanf("%d %d %f", &x, &y, &r);
+printf("circle(%d %d, %f", x, y, r);
+printf(")");
+circle(x, y ,r);
+ 
+
+
+}  
